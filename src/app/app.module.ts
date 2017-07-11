d1adf0178e3dcc52be3cb843ad0147dbef33c2b1
Ivange Larry <ivangelarry@gmail.com>
2017-07-11 08:59:09 +0100

    Add report-editor component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TinymceModule } from 'angular2-tinymce';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrandComponent } from './brand/brand.component';
 import { FooterComponent } from './footer/footer.component';
+import { ReportEditorComponent } from './report-editor/report-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BrandComponent,
-    FooterComponent
+    FooterComponent,
+    ReportEditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TinymceModule.withConfig({}),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
