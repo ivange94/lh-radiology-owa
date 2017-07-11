b10d3f6327a355493c8fbfcd84bf622cf835702d
Ivange Larry <ivangelarry@gmail.com>
2017-07-11 10:54:29 +0100

    Add report-medatadata component

---
+++ src/app/report-metadata/report-metadata.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-report-metadata',
+  templateUrl: './report-metadata.component.html',
+  styleUrls: ['./report-metadata.component.css']
+})
+export class ReportMetadataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
