2702b402d06d7cdaf6a67410cb0d683c473e6e30
Ivange Larry <ivangelarry@gmail.com>
2017-09-01 23:02:04 +0100

    Add service method to fetch orders with pagination

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,22 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Order } from '../models/order';
-import {orders} from "../mock-db";
+import {HttpClient, HttpParams} from "@angular/common/http";
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class OrderService {
 
-  constructor() { }
+  private baseUrl = "/openmrs/ws/rest/v1/radiologyorder";
 
-  getOrders(): Promise<Order[]> {
-    return Promise.resolve(orders);
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  fetch(startIndex: number, limit: number): Observable<Order[]> {
+    let params = new HttpParams();
+    params = params.set('startIndex',startIndex.toString()).set('limit',limit.toString()).set('v', 'full').set('totalCount', 'true');
+
+    return this.http.get(this.baseUrl, {
+      params
+    })
+      .map(res => res['results'] as Order[] || [])
+      .catch(this.handleError);
   }
 
-  getOrder(accessionNumber: number): Promise<Order> {
-    return this.getOrders().then(orders => orders.find(order => order.accessionNumber === accessionNumber));
+  private handleError(error: any): Promise<any> {
+    console.log('An error occured ', error);
+    return Promise.reject(error.message || error);
   }
-
-  addOrder(order: Order): void {
-    this.getOrders().then(orders => orders.push(order));
-  }
-
 }
